e8abb245582fb63e32212dc89d9ef5a628bbac1e
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-06 22:49:58 +0300

    Make backend for universe

--- src/app/views/stars.py
+++ src/app/views/stars.py
@@ -4,7 +4,7 @@ from flask import request, jsonify
 
 from ..app import app
 
-from stars import Multiverse, Universe, Supercluster, Star
+from stars import Multiverse, Universe, Supercluster, Galaxy, Star
 
 
 @app.route('/api/v1.0/multiverse', methods=['GET'])
@@ -14,7 +14,6 @@ def get_multiverse(id=None):
     return jsonify({ 'multiverse': multiverse.as_dict() })
 
 
-# new Thing("universe",["supercluster,10-30"]);
 @app.route('/api/v1.0/universe', methods=['GET'])
 @app.route('/api/v1.0/universe-<id>', methods=['GET'])
 def get_universe(id=None):
@@ -22,7 +21,6 @@ def get_universe(id=None):
     return jsonify({'universe': universe.as_dict()})
 
 
-# new Thing("supercluster",["galaxy,10-30"],"galactic supercluster");
 @app.route('/api/v1.0/cluster', methods=['GET'])
 @app.route('/api/v1.0/cluster-<id>', methods=['GET'])
 def get_cluster(id=None):
@@ -30,18 +28,13 @@ def get_cluster(id=None):
     return jsonify({'cluster': cluster.as_dict()})
 
 
-# new Thing("galaxy",["galaxy center","galaxy arm,2-6"]);
-# new Thing("galaxy arm",["galactic life,5%","dyson sphere,4%","dyson sphere,2%","star system,20-50","nebula,0-12","black hole,20%","black hole,20%"],"arm");
-# new Thing("galaxy center",["black hole","galactic life,10%","dyson sphere,4%","dyson sphere,2%","star system,20-50","nebula,0-12"],"galactic center");
 @app.route('/api/v1.0/galaxy', methods=['GET'])
 @app.route('/api/v1.0/galaxy-<id>', methods=['GET'])
 def get_galaxy(id=None):
-    cluster = Supercluster(id)
-    return jsonify({'cluster': cluster.as_dict()})
+    galaxy = Galaxy(id)
+    return jsonify({'galaxy': galaxy.as_dict()})
 
 
-# new Thing("nebula",["galactic life,15%","star,2%","star,2%","star,2%","interstellar cloud,1-6"]);
-# new Thing("interstellar cloud",["helium","hydrogen","carbon,80%","water,5%","ammonia,5%","nitrogen,5%","iron,5%","sulfur,5%","oxygen,15%"],[["a bright pink","a faint","a fading","a pale","a fluo","a glowing","a green","a bright green","a dark brown","a brooding","a magenta","a bright red","a dark red","a blueish","a deep blue","a turquoise","a teal","a golden","a multicolored","a silver","a dramatic","a luminous","a colossal","a purple","a gold-trimmed","an opaline","a silvery","a shimmering"],[" "],["interstellar cloud"]]);
 @app.route('/api/v1.0/nebula', methods=['GET'])
 @app.route('/api/v1.0/nebula-<id>', methods=['GET'])
 def get_nebula(id=None):
@@ -49,7 +42,6 @@ def get_nebula(id=None):
     return jsonify({'cluster': cluster.as_dict()})
 
 
-# new Thing("star system",["star","star,3%","visitor planet,5%","future planet,10%","future planet,10%","terraformed planet,50%","terraformed planet,20%","terraformed planet,10%","medieval planet,30%","medieval planet,20%","ancient planet,50%","ancient planet,30%","ancient planet,10%","barren planet,60%","barren planet,40%","barren planet,20%","gas giant,60%","gas giant,40%","gas giant,20%","gas giant,10%","asteroid belt,0-2"]);
 @app.route('/api/v1.0/stars', methods=['GET'])
 def get_stars():
     count = int(request.args.get('count', 200))
--- src/stars.py
+++ src/stars.py
@@ -102,10 +102,164 @@ class Universe(Generated):
 
 
 class Supercluster(Generated):
-    names = ['Supercluster']
+    names = ['Galactic Supercluster']
 
+    def __init__(self, id=None, name=None, width=8, height=8, depth=8):
+        Generated.__init__(self, id, name)
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+        self.galaxies = [Location(
+            Galaxy,
+            x=random.randrange(self.width),
+            y=random.randrange(self.height),
+            z=random.randrange(self.depth),
+        ) for i in range(random.randint(10, 30))]
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'width': self.width,
+            'height': self.height,
+            'depth': self.depth,
+            'galaxies': [galaxy.as_dict() for galaxy in self.galaxies],
+        }
+
+
+class GalaxyCenter(Generated):
+    names = ['Galactic Center']
+    """
+    "black hole",
+    "galactic life,10%",
+    "dyson sphere,4%",
+    "dyson sphere,2%",
+    "star system,20-50",
+    "nebula,0-12"
+    """
+
+
+class GalaxyArm(Generated):
+    names = ['Arm']
+    """
+    "galactic life,5%",
+    "dyson sphere,4%",
+    "dyson sphere,2%",
+    "star system,20-50",
+    "nebula,0-12",
+    "black hole,20%",
+    "black hole,20%"
+    """
+
+class Galaxy(Generated):
+    names = ['Galaxy']
+
+    def __init__(self, id=None, name=None, radius=8):
+        Generated.__init__(self, id, name)
+        self.radius = radius
+
+        self.center = GalaxyCenter(random.randrange(1024))
+        self.arms = [GalaxyArm(random.randrange(1024)) for i in range(random.randint(2, 6))]
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'radius': self.radius,
+            'center': self.center.as_dict(),
+            'arms': [arm.as_dict() for arm in self.arms],
+        }
+
+
+class Nebula(Generated):
+    names = ["Nebula"]
+    """
+    [
+    "galactic life,15%",
+    "star,2%",
+    "star,2%",
+    "star,2%",
+    "interstellar cloud,1-6"
+    ]
+    """
+
+
+class InterstellarCloud(Generated):
+    """
+    [
+    "helium",
+    "hydrogen",
+    "carbon,80%",
+    "water,5%",
+    "ammonia,5%",
+    "nitrogen,5%",
+    "iron,5%",
+    "sulfur,5%",
+    "oxygen,15%"
+    ],
+
+    [
+        "a bright pink",
+        "a faint",
+        "a fading",
+        "a pale",
+        "a fluo",
+        "a glowing",
+        "a green",
+        "a bright green",
+        "a dark brown",
+        "a brooding",
+        "a magenta",
+        "a bright red",
+        "a dark red",
+        "a blueish",
+        "a deep blue",
+        "a turquoise",
+        "a teal",
+        "a golden",
+        "a multicolored",
+        "a silver",
+        "a dramatic",
+        "a luminous",
+        "a colossal",
+        "a purple",
+        "a gold-trimmed",
+        "an opaline",
+        "a silvery",
+        "a shimmering"
+    ],
+    " "
+    "interstellar cloud"
+    """
 
 class Star:
+    """
+    "star system",
+    [
+    "star",
+    "star,3%",
+    "visitor planet,5%",
+    "future planet,10%",
+    "future planet,10%",
+    "terraformed planet,50%",
+    "terraformed planet,20%",
+    "terraformed planet,10%",
+    "medieval planet,30%",
+    "medieval planet,20%",
+    "ancient planet,50%",
+    "ancient planet,30%",
+    "ancient planet,10%",
+    "barren planet,60%",
+    "barren planet,40%",
+    "barren planet,20%",
+    "gas giant,60%",
+    "gas giant,40%",
+    "gas giant,20%",
+    "gas giant,10%",
+    "asteroid belt,0-2"
+    ]
+    """
     def __init__(self, width=4000, height=4000, depth=4000):
         self.x = random.randrange(-width/2, width/2)
         self.y = random.randrange(-height/2, height/2)
