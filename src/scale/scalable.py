34d3beb52aa3083d3a452166eac5203e45fb19d6
d2emon <d2emonium@gmail.com>
2018-08-08 18:04:54 +0300

    Scalable Size

--- src/scale/scalable.py
+++ src/scale/scalable.py
@@ -1,18 +1,27 @@
-class Scalable:
-    def __init__(self, name, size, scale=1):
-        while size < 1:
-            if size <= 0:
-                break
-            scale -= 1
-            size *= 10
-
-        while size > 10:
-            scale += 1
-            size /= 10
-
-        self.name = name
+class ScalableSize:
+    def __init__(self, size, scale=0):
         self.size = size
         self.scale = scale
 
+    def adjust(self):
+        while self.size < 1:
+            if self.size <= 0:
+                break
+            self.scale -= 1
+            self.size *= 10
+
+        while self.size > 10:
+            self.scale += 1
+            self.size /= 10
+
     def __repr__(self):
-        return "{} ({}*10^{}m)".format(self.name, self.size, self.scale)
+        return "{}*10^{}m".format(self.size, self.scale)
+
+
+class Scalable:
+    def __init__(self, name, size, scale=0):
+        self.name = name
+        self.size = ScalableSize(size, scale)
+
+    def __repr__(self):
+        return "{} ({})".format(self.name, self.size)
