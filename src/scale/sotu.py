92920fa9d8831301f269dbc8aed161ea192fff44
d2emon <d2emonium@gmail.com>
2018-08-09 15:44:03 +0300

    Scales of the Universe

--- src/scale/sotu.py
+++ src/scale/sotu.py
@@ -41,6 +41,7 @@ SOTU = [
     Scalable("Верхний Кварк", 0.1, -21),
 
     Scalable("Высокоэнергетическое Нейтрино", 15, -21),
+    Scalable("Преон", 1, -21),
 
     Scalable("Нижний Кварк", 30, -21),
 
@@ -48,6 +49,7 @@ SOTU = [
     Scalable("Странный Кварк", 0.4, -18),
     Scalable("Истинный Кварк", 1, -18),
     Scalable("Прелестный Кварк", 1, -18),
+    Scalable("Ядро электрона", 1, -18),
 
     Scalable("Слабые Силы", 10, -18),
 
@@ -58,13 +60,18 @@ SOTU = [
     Scalable("Электрон (Классический)", 5, -15),
     Scalable("Ядро Хлора", 6, -15),
     Scalable("Ядро Урана", 15, -15),
+    Scalable("Атомное Ядро (Среднее)", 10, -15),
 
     Scalable("Длина Волны Гамма-излучения", 1, -12),
+
+    Scalable("Electron Compton Wavelength", 2, -12),
+
     Scalable("Атом Гелия", 25, -12),
     Scalable("Атом Водорода", 31, -12),
 
     Scalable("Молекула Воды", 0.28, -9),
     Scalable("Атом Углерода", 0.34, -9),
+    Scalable("Атом Серы", 0.1, -9),
     Scalable("Атом Цезия", 0.23, -9),
 
     Scalable("Alpha Helix", 0.5, -9),
@@ -86,8 +93,10 @@ SOTU = [
 
     Scalable("Вирус Гепатита B", 42, -9),
     Scalable("ВИЧ", 90, -9),
+    Scalable("Клеточная Мембрана", 10, -9),
     Scalable("Длина Волны Ультрафиолетового Света", 60, -9),
 
+    Scalable("Глубина Пита Комакт-Диска", 0.12, -6),
     Scalable("Наибольшее Разрешение Оптического Микроскопа", 0.2, -6),
     Scalable("Бактериофаг", 0.2, -6),
 
@@ -95,6 +104,7 @@ SOTU = [
 
     Scalable("Самый Большой Вирус", 0.44, -6),
     Scalable("Длина Волны Фиолетового Света", 0.4, -6),
+    Scalable("Наибольшая частица, способная проникнуть сквозь хирургическую маску", 1, -6),
 
     Scalable("E. coli", 2, -6),
 
@@ -112,17 +122,20 @@ SOTU = [
     Scalable("Thou", 25.4, -6),
     Scalable("Twip", 17.6, -6),
 
+    Scalable("Толщина Акриловой Нити", 12, -6),
     Scalable("Толщина Шелковой Нити", 15, -6),
     Scalable("Клетка Кожи", 35, -6),
     Scalable("Длина Волны Инфракрасного Света", 15, -6),
 
     Scalable("Slit Particle", 50, -6),
+    Scalable("Pollen Grain", 50, -6),
 
     Scalable("Разрешение Человеческого Глаза", 0.1, -3),
     Scalable("Человеческая Яйцеклетка", 0.12, -3),
     Scalable("Толщина Человеческого Волоса", 0.1, -3),
     Scalable("Толщина Бумаги", 0.15, -3),
 
+    Scalable("Ciliate Protist", 0.2, -3),
     Scalable("Paramecium", 0.2, -3),
 
     Scalable("Амеба", 0.3, -3),
@@ -131,6 +144,8 @@ SOTU = [
     Scalable("Кристалл Соли", 0.5, -3),
     Scalable("Крупнейшая Бактерия", 0.75, -3),
     Scalable("Песчинка", 0.5, -3),
+    Scalable("Толщина Человеческой Кожи", 0.5, -3),
+    Scalable("Толщина Кредитной Карты", 0.7, -3),
 
     Scalable("Duckweed", 2, -3),
 
@@ -139,36 +154,50 @@ SOTU = [
     Scalable("Град", 5, -3),
     Scalable("Рисовое Зерно", 5, -3),
     Scalable("Толщина Грифеля", 0.7, -3),
+    Scalable("Толщина Человеческой Кожи (подошвы)", 4, -3),
+    Scalable("Зерно Сезама", 3, -3),
+    Scalable("Микрочип", 4, -3),
 
-    Scalable("Кофейное Зерно", 10, -1),
-    Scalable("Стеклянный Шарик", 15, -1),
-    Scalable("Длина Волны Микроволновки", 10, -1),
-    Scalable("Квадратный Дюйм", 25, -1),
-    Scalable("Пенни США", 19, -1),
-    Scalable("Перепелиное Яйцо", 30, -1),
-    Scalable("Дождевой Червь", 40, -1),
-    Scalable("Спичка", 50, -1),
-    Scalable("Куриное Яйцо", 55, -1),
+    Scalable("Кофейное Зерно", 10, -3),
+    Scalable("Яйцо Колибри", 12, -3),
+    Scalable("Стеклянный Шарик", 15, -3),
+    Scalable("Длина Волны Микроволновки", 10, -3),
+    Scalable("Квадратный Дюйм", 25, -3),
+    Scalable("Пенни США", 19, -3),
+    Scalable("Снежинка", 10, -3),
+    Scalable("Перепелиное Яйцо", 30, -3),
+    Scalable("Дождевой Червь", 40, -3),
+    Scalable("Спичка", 50, -3),
+    Scalable("Куриное Яйцо", 55, -3),
+    Scalable("Кубик Рубика", 55, -3),
 
+    Scalable("Стакан", 0.12, 0),
+    Scalable("Страусиное Яйцо", 0.15, 0),
     Scalable("Землеройка", 0.1, 0),
     Scalable("Колибри", 0.1, 0),
     Scalable("Это Окно", 0.2, 0),
     Scalable("Чайник Рассела", 0.25, 0),
     Scalable("Крупнейшая Градина", 0.2, 0),
+    Scalable("Крупнейшая Снежинка", 0.38, 0),
     Scalable("Баскетбольный Мяч", 0.24, 0),
     Scalable("Дюймовая Линейка", 0.3, 0),
     Scalable("Дронт", 1, 0),
-    Scalable("Пляжный мяч", 0.8, 0),
+    Scalable("Маленький Пляжный мяч", 0.5, 0),
+    Scalable("Большой Пляжный мяч", 1, 0),
     Scalable("Раффлезия", 1, 0),
     Scalable("Человек", 1.7, 0),
     Scalable("Гигантский Дождевой Червь", 3, 0),
     Scalable("Подсолнечник", 2.5, 0),
     Scalable("Странствующий Альбатрос", 3, 0),
     Scalable("FM Волна", 1, 0),
+    Scalable("Машина", 4, 0),
+    Scalable("Высота дома", 2.5, 0),
     Scalable("Японский Краб", 3, 0),
+    Scalable("Автобус", 12, 0),
     Scalable("Слон", 5, 0),
     Scalable("Жираф", 6, 0),
     Scalable("Тирранозавр Рекс", 7, 0),
+    Scalable("Анаконда", 8, 0),
     Scalable("Лунный Модуль Аполло", 9, 0),
     Scalable("Сагуаро", 14, 0),
     Scalable("Дом", 15, 0),
@@ -195,13 +224,15 @@ SOTU = [
     Scalable("Дамба Гувера", 0.22, 3),
 
     Scalable("Half Dome", 0.41, 3),
+    Scalable("International Commercial Centre", 0.484, 3),
+    Scalable("Sears Tower", 0.442, 3),
 
     Scalable("Ватикан", 0.8, 3),
     # Бурж Халифа
     Scalable("Водопад Анхель", 0.979, 3),
 
     Scalable("Boeing Everett Factory", 1, 3),
-
+    Scalable("Stanford Linear Accelerator", 3, 3),
     Scalable("Uluru", 3, 3),
     Scalable("Cruithne", 5, 3),
 
@@ -220,11 +251,17 @@ SOTU = [
     Scalable("Нейтронная Звезда", 24, 3),
     Scalable("Род Айленд", 75, 3),
 
+    Scalable("Ganymed", 32, 3),
+    Scalable("Bianca", 54, 3),
+    Scalable("Belinda", 80, 3),
     Scalable("Nix", 80, 3),
 
     Scalable("Гидра", 0.1, 6),
 
     Scalable("Dysnomia", 0.15, 6),
+    Scalable("Galatea", 0.16, 6),
+    Scalable("Nereid", 0.34, 6),
+    Scalable("Miranda", 0.47, 6),
 
     Scalable("Бруней", 0.12, 6),
     Scalable("Западная Вирджиния", 0.4, 6),
@@ -252,6 +289,7 @@ SOTU = [
     Scalable("Ио", 3.6, 6),
     Scalable("Луна", 3.5, 6),
     Scalable("Титан", 5.2, 6),
+    Scalable("Титания", 1.6, 6),
     Scalable("Меркурий", 4.9, 6),
     Scalable("США", 4.2, 6),
     Scalable("Марс", 6.8, 6),
@@ -271,6 +309,7 @@ SOTU = [
 
     Scalable("Проксима Центавра", 0.2, 9),
     Scalable("От Земли До Луны", 0.38, 9),
+    Scalable("Самая длинная радиоволна", .1, 9),
 
     Scalable("Luyten's Star", 0.42, 9),
     Scalable("Kapteyn's Star", 0.42, 9),
@@ -292,13 +331,16 @@ SOTU = [
     Scalable("Капелла", 17, 9),
 
     Scalable("Albireo", 22, 9),
+    Scalable("VV Cephei B", 20, 9),
 
     Scalable("Арктур", 36, 9),
     Scalable("Полярная", 40, 9),
     Scalable("Альдебаран", 64, 9),
     Scalable("Альнитак", 84, 9),
-    Scalable("Расстояние от Земли до Солнца", 0.15, 12),
+    Scalable("Расстояние от Меркурия до Солнца", 60, 9),
     Scalable("Ригель", 97, 9),
+
+    Scalable("Расстояние от Земли до Солнца", 0.15, 12),
 
     Scalable("Gacrux", 0.16, 12),
 
@@ -309,6 +351,10 @@ SOTU = [
     Scalable("Pistol Star", 0.47, 12),
     Scalable("Антарес", 0.97, 12),
     Scalable("S Doradus", 0.77, 12),
+    Scalable("V382 Carinei", 1, 12),
+    Scalable("V838 Monocerotis", 1.1, 12),
+
+    Scalable("Расстояние от Юпитера до Солнца", 0.78, 12),
 
     Scalable("Бетельгейзе", 1.3, 12),
     Scalable("Мю Цефея", 1.9, 12),
@@ -318,10 +364,12 @@ SOTU = [
 
     Scalable("VY Малых Псов", 3, 12),
 
+    Scalable("Herschel's Garnet Star", 1.9, 12),
     Scalable("KY Cygni", 2, 12),
     Scalable("V354 Cephei", 2.1, 12),
 
     Scalable("Расстояние от Нептуна до Солнца", 4.5, 12),
+    Scalable("Расстояние от Плутона до Солнца", 5.9, 12),
     Scalable("Пояс Койпера", 15, 12),
     Scalable("Расстояние от Вояджера 1 до Земли", 17, 12),
     Scalable("Световой День", 26, 12),
@@ -333,6 +381,8 @@ SOTU = [
     Scalable("Stingray Nebula", 0.7, 15),
     Scalable("Gomez's Hamburger", 2.5, 15),
 
+    Scalable("Расстояние от Проксимы Центавра до Альфы Центавра A", 1.5, 15),
+    Scalable("Облако Оорта", 1.5, 15),
     Scalable("Туманность Кошачий Глаз", 2.5, 15),
 
     Scalable("Hourglass Nebula", 3, 15),
@@ -369,6 +419,9 @@ SOTU = [
     Scalable("Eagle Nebula", 0.7, 18),
     Scalable("Cave Nebula", 0.7, 18),
     Scalable("Rosette Nebula", 1, 18),
+    Scalable("Solar System's Solar Neighborhood", 0.4, 18),
+    Scalable("M41 Globular Cluster", 0.5, 18),
+    Scalable("M54 Globular Cluster", 1, 18),
 
     Scalable("Омега Центавра", 1.5, 18),
 
@@ -380,6 +433,7 @@ SOTU = [
     Scalable("Canis Major Dwarf Galaxy", 50, 18),
 
     Scalable("Малое Магелланово Облако", 70, 18),
+    Scalable("Джет из сверхмассивной черной дыры в центре галактики M87 в Скоплении Девы", 50, 18),
 
     Scalable("Sagittarius Dwarf Galaxy", 0.1, 21),
     Scalable("Canis Venatici Dwarf Galaxy I", 65, 18),
@@ -387,11 +441,13 @@ SOTU = [
     Scalable("Большое Магелланово Облако", 0.14, 21),
 
     Scalable("NGC 3310", 0.22, 21),
+    Scalable("NGC 7714", 0.35, 21),
     Scalable("Sombrero Galaxy", 0.5, 21),
     Scalable("Triangulum Galaxy", 0.5, 21),
 
     Scalable("Млечный Путь", 1.2, 21),
 
+    Scalable("Black Eye Galaxy", 0.85, 21),
     Scalable("Andromeda Galaxy", 1.5, 21),
     Scalable("Pinwheel Galaxy", 1.7, 21),
     Scalable("Cartwheel Galaxy", 1.5, 21),
