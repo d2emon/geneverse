4f395697f1ac2a9a17a395ba2b7f782d5911fd62
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-15 17:40:31 +0300

    Add Void

--- src/app/views/space.py
+++ src/app/views/space.py
@@ -7,24 +7,55 @@ from app import app
 from genimage import universe, save_image
 
 
-class Star:
-    def __init__(self, x, y, z=0, size=1, brightness=255):
+class SpaceObject:
+    def __init__(self, x, y, z=0, size=1):
         self.x = x
         self.y = y
         self.z = z
         self.size = size
+
+
+class SuperVoid(SpaceObject):
+    def __init__(self, x, y, z=0, size=255):
+        super().__init__(x, y, z, size)
+
+    def get_filter(self):
+        def f(item):
+            if (item.x - self.x) ** 2 > self.size ** 2:
+                return True
+            if (item.y - self.y) ** 2 > self.size ** 2:
+                return True
+            if (item.z - self.z) ** 2 > self.size ** 2:
+                return True
+            return False
+        return f
+
+
+class Star(SpaceObject):
+    def __init__(self, x, y, z=0, size=1, brightness=255):
+        super().__init__(x, y, z, size)
         self.brightness = brightness
 
+
+def random_star(size, star_size):
+    return Star(
+        x=random.randrange(size),
+        y=random.randrange(size),
+        z=random.randrange(size),
+        size=star_size,
+        brightness=random.randrange(64, 255)
+    )
 
 @app.route('/space/img-<int:size>.png')
 def get_space_img(size):
     star_max_size = 3
-    stars_count = 2048
-    stars = [Star(
+    stars_count = 4096
+    void = SuperVoid(
         x=random.randrange(size),
         y=random.randrange(size),
         z=random.randrange(size),
-        size=star_max_size,
-        brightness=random.randrange(64, 255)
-    ) for i in range(stars_count)]
-    return send_file(save_image(universe(size, size, stars)), mimetype='image/jpeg')
+        size=400
+    )
+    stars = [random_star(size, star_max_size) for _ in range(stars_count)]
+    filtered = filter(void.get_filter(), stars)
+    return send_file(save_image(universe(size, size, filtered)), mimetype='image/jpeg')
