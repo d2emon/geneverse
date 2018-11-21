b8019666c9d268aba51588ce2471573bc690223d
Dmitry Kutsenko <d2emonium@gmail.com>
2018-11-21 17:52:41 +0300

    Simple doll

--- nested_frontend/src/router/index.js
+++ nested_frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Nested from '@/components/Nested'
+import Doll from '@/components/Doll'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Nested',
       component: Nested
+    },
+    {
+      path: '/doll',
+      name: 'Doll',
+      component: Doll
     }
   ]
 })
